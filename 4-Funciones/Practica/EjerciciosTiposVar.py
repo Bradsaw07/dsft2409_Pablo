339cd32f1cd7867165ce9b103781f4a1ab831885
Bradsaw07 <pablohalo10@gmail.com>
2024-10-03 13:56:17 +0200

    Mas ejemplos y creacion de ejerciciosvariables.py

---
+++ 4-Funciones/Practica/EjerciciosTiposVar.py
@@ -0,0 +1,16 @@
+biblioteca = [{},{},{}]
+
+def funcionPrueba():
+    a = 1
+    b = 2
+    c = 3
+    print(a,b,c)
+    print(biblioteca)
+def funcionPrueba2():
+    funcionPrueba3()
+
+def funcionPrueba3():
+    print("Hello")
+
+funcionPrueba()
+funcionPrueba2()
